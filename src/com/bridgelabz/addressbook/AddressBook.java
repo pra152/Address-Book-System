67c989ebb893e98734cbcb7241f582facfe355f3
pra152 <pravatdas211@gmail.com>
2021-08-12 10:20:31 +0530

    Added :create a Contacts in Address Book

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,8 +1,16 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
+
+import com.bridgelabz.addressbook.dto.Person;
+
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("...........Welcome to Address Book Management System Development......");
+		ArrayList<Person> personList = new ArrayList<>();
+		Person person = new Person();
+		personList.add(person);
+		System.out.println(personList);
 	}
 
 }
